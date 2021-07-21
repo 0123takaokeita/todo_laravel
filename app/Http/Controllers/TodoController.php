15a3431ef98994eaa70014605fd4d57f16a6fe74
Keita Takao <keita.takao.1112@gmail.com>
2021-07-21 22:36:45 +0900

    一覧追加

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    //一覧画面を表示
+    public function index(){
+        $todos = Todo::all();
+        return view('todo.index',compact('todos'));
+    }
 }
