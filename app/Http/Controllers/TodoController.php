e9f64ed4c359edbad0efc6237c9e46ee7c307c23
Keita Takao <keita.takao.1112@gmail.com>
2021-07-22 11:40:59 +0900

    詳細機能実装

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use PDO;
 
 class TodoController extends Controller
 {
@@ -15,5 +16,26 @@ class TodoController extends Controller
     //投稿画面を表示
     public function create(){
         return view('todo.create');
+    }
+
+    //メッセージの返却
+    public function store(Request $request){
+        // インスタンス作成。
+        $todo = new Todo();
+        // titleカラムにrequestのtitleを保存。
+        $todo->title = $request -> input('title');
+        //変数を保存。
+        $todo->save();
+
+        return redirect('todos')->with(
+            'status',
+            $todo->title . 'を登録しました。'
+        );
+    }
+
+    //詳細画面の返却
+    public function show($id){
+        $todo = Todo::findOrFail($id);
+        return view('todo.show',compact('todo'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/todo', TodoController::class);
+Route::resource('/todos', TodoController::class);
